225ad2ea2571963bb1cd454c8471ed91505f77ce
GURU PRASANTH D <147241556+jackiestroffguru@users.noreply.github.com>
2023-10-16 21:16:17 +0530

    Create Code4.py

---
+++ Day 4/Code4.py
@@ -0,0 +1,12 @@
+print("Welcome to your adventure simulator. I am going to ask you a bunch of questions and then create an epic story with you as the star!")
+
+name = input("What is your name?")
+enemy = input("What is your worst enemy’s name?")
+superpower = input("What is your superpower?")
+home = input("Where do you live?")
+food = input("What is your favorite food?")
+
+print("Hello",name,"Your ability to ",superpower ,"will make sure you never have to look at",enemy, "again.Go eat,food", "as you walk down the streets of",home, "and use", superpower, "for good and not evil!")
+
+#Print using The color code
+print("Uh, oh, David! you've been given a", "\033[31m", "Warning", "\033[0m","for being a bad person")
