34c5dd3088322b7b24a6ca59c877131e6b80ecad
GURU PRASANTH D <147241556+jackiestroffguru@users.noreply.github.com>
2023-10-23 10:33:06 +0530

    Create elif.py

---
+++ Day 6/elif.py
@@ -0,0 +1,18 @@
+print("MY LOGIN SYSTEM")
+print("****************")
+
+username = input("What is your username?")
+password = input("What is your password?")
+
+if username == "Guru" and password == "22gp":
+  print("Welcome, Guru! What ya lovely accent you have, you could have Charmed your way in here even without a password.\n")
+  print("Have a great day.\n")
+
+  print("Don't forgot to wear a hat in the sun!")
+
+elif username == "Groover" and password == "R34p":
+  print("Hi Groover! Love your hair today!")
+elif username == "Billa" and password == "Sma!!":
+  print("Yo! Billa! What up?!")
+else:
+  print("You do not have access. Go away!")
